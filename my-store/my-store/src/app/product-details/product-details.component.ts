b8d27d89475a533ad52ba0203dcb2e4dfded6a0a
Nikolay Nikolaev <nikolay@we-lovedigi.com>
2024-04-01 19:00:39 +0300

    complete my-shop tutorial

---
+++ my-store/my-store/src/app/product-details/product-details.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Product, products } from '../products';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
+})
+export class ProductDetailsComponent implements OnInit {
+  product?: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+
+  addToCart() {
+    if (typeof this.product !== 'undefined') {
+      this.cartService.addToCart(this.product);
+      window.alert('Your product has been added to the cart!');
+    }
+  }
+}
